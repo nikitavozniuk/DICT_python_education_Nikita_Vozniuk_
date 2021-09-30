b503f165dfb8fb9539a232c99336920b23498152
Nikita Vozniuk <n.i.voznyuk@student.khai.edu>
2021-09-30 23:29:48 +0300

    Hangman 5-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -7,11 +7,29 @@ The game will be available soon.
 
 words = ["python", "java", "javascript", "php", "c++"]
 word = random.choice(words)
-hint = word[:3] + "-" * (len(word) - 3)
 
-answer = str(input("Guess the word {}: ".format(hint)))
 
-if answer == word:
-    print("You survived!")
-else:
-    print("You lost!")
+def start():
+    attempts = 8
+    guess = word.replace(word, "-" * len(word))
+    while word != guess:
+        if attempts == 0:
+            return
+
+        guess_list = list(guess)
+        print(guess)
+        letter = str(input("Input a letter: "))
+
+        if letter not in word:
+            print("That letter doesn't appear in the word")
+        else:
+            for index, item in enumerate(word):
+                if letter == item:
+                    guess_list[index] = item
+                    guess = "".join(guess_list)
+        attempts -= 1
+
+
+start()
+print("Thanks for playing!")
+print("We'll see how well you did in the next stage")
