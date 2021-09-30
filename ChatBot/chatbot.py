d9993348ad8c7e4a9e0e475fe52126adbfb08012
Nikita Vozniuk <n.i.voznyuk@student.khai.edu>
2021-09-30 21:53:50 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -3,3 +3,7 @@ birth_year = 2021
 
 print("Hello! My name is {}".format(bot_name))
 print("I was created in {}".format(birth_year))
+
+print("Please, remind me your name.")
+name = str(input())
+print("What a great name you have, {}!".format(name))
