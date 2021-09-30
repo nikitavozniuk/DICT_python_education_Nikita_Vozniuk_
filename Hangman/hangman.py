d0681febd867fb99d75e4bd033cb0771af10b944
Nikita Vozniuk <n.i.voznyuk@student.khai.edu>
2021-09-30 22:43:23 +0300

    Hangman 2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,3 +2,11 @@ print("""
 HANGMAN
 The game will be available soon.
 """)
+
+word = "dict"
+answer = str(input("Guess the word: "))
+
+if answer == word:
+    print("You survived!")
+else:
+    print("You lost!")
