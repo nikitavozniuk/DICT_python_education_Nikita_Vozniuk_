b90bcbd01ef3d17f089fb8ddc4a7b7725511f0d6
Nikita Vozniuk <n.i.voznyuk@student.khai.edu>
2021-09-30 23:01:58 +0300

    Hangman 4-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -7,8 +7,9 @@ The game will be available soon.
 
 words = ["python", "java", "javascript", "php", "c++"]
 word = random.choice(words)
+hint = word[:3] + "-" * (len(word) - 3)
 
-answer = str(input("Guess the word: "))
+answer = str(input("Guess the word {}: ".format(hint)))
 
 if answer == word:
     print("You survived!")
