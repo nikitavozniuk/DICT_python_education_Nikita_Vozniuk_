0e265128fac19cf4905e81e04e53fe303889cb2f
Nikita Vozniuk <n.i.voznyuk@student.khai.edu>
2021-12-01 22:10:21 +0200

    Complete TicTacToe

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,97 +1,148 @@
-cells = list(input("Enter cells: "))
-count = 0
+in_range = [int(i) for i in range(1, 4)]
 
-X = cells.count("X")
-O = cells.count("O")
-cells_ = cells.count("_")
+def tic_tac_toe():
+    board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+    end = False
 
+    def draw():
+        print('---------')
+        print('| {} {} {} |'.format(board[0][0], board[0][1], board[0][2]))
+        print('| {} {} {} |'.format(board[1][0], board[1][1], board[1][2]))
+        print('| {} {} {} |'.format(board[2][0], board[2][1], board[2][2]))
+        print('---------')
+        print()
 
-def printCells():
-    print("---------")
-    print("| ", end="")
-    for item in cells[:3]:
-        print(item, end=" ")
-    print("|")
-    print("| ", end="")
-    for item in cells[3:6]:
-        print(item, end=" ")
-    print("|")
-    print("| ", end="")
-    for item in cells[6:9]:
-        print(item, end=" ")
-    print("|")
-    print("---------")
-
-
-printCells()
-
-
-def checkBoard():
-    global count
-    if cells[0] == cells[4] == cells[8] != "_":
-        count += 1
-    if cells[2] == cells[5] == cells[8] != "_":
-        count += 1
-    if cells[1] == cells[4] == cells[7] != "_":
-        count += 1
-    if cells[2] == cells[4] == cells[6] != "_":
-        count += 1
-    if cells[0] == cells[3] == cells[6] != "_":
-        count += 1
-    if cells[6] == cells[7] == cells[8] != "_":
-        count += 1
-    if cells[0] == cells[1] == cells[2] != "_":
-        count += 1
-    if cells[3] == cells[4] == cells[5] != "_":
-        count += 1
-
-
-checkBoard()
-
-
-def validateInput(coordinates):
-    if type(coordinates[0]) == str() and type(coordinates[1]) == str():
-        print('You should enter numbers!')
-        return inputCells()
-
-    if 0 < coordinates[0] < 4 and 0 < coordinates[1] < 4:
-        if coordinates == (1, 1):
-            if cells[0] == "_":
-                cells[0] = "X"
-                printCells()
-    else:
-        print('Coordinates should be from 1 to 3!')
-        return inputCells()
-
-
-def inputCells():
-    inp = tuple(int(x.strip()) for x in input('Enter the coordinates: ').split(' '))
-    validateInput(inp)
-
-
-if count == 0 or count == 1:
-    if X == O + 1 or X == O - 1 or X == O:
-        if cells[0] == cells[1] == cells[2] != "_":
-            print(cells[0] + " wins")
-        elif cells[3] == cells[4] == cells[5] != "_":
-            print(cells[3] + " wins")
-        elif cells[6] == cells[7] == cells[8] != "_":
-            print(cells[6] + " wins")
-        elif cells[0] == cells[4] == cells[8] != "_":
-            print(cells[0] + " wins")
-        elif cells[2] == cells[4] == cells[6] != "_":
-            print(cells[2] + " wins")
-        elif cells[1] == cells[4] == cells[7] != "_":
-            print(cells[4] + " wins")
-        elif cells[2] == cells[5] == cells[8] != "_":
-            print(cells[5] + " wins")
-        elif cells[0] == cells[3] == cells[6] != "_":
-            print(cells[3] + " wins")
-        elif cells_ > 0:
-            inputCells()
+    def p1():
+        row, col = choose_number()
+        if board[row][col] == "X" or board[row][col] == "O":
+            print("\nYou can't go there. Try again")
+            p1()
         else:
-            print("Draw!")
-    else:
-        print("Impossible!")
-else:
-    print("Impossible!")
+            board[row][col] = "X"
+
+    def p2():
+        row, col = choose_number()
+        if board[row][col] == "X" or board[row][col] == "O":
+            print("\nYou can't go there. Try again")
+            p2()
+        else:
+            board[row][col] = "O"
+
+    def choose_number():
+        while True:
+            try:
+                row, col = input().split(' ')
+                row = int(row)
+                col = int(col)
+            except ValueError:
+                print('You should enter numbers!')
+                continue
+            if row and col not in in_range:
+                print('Coordinates should be from 1 to 3!')
+            return row-1, col-1
+
+    def check_board():
+        count = 0
+        if board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+
+        if board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][1] == "O" and board[1][1] == "X" and board[2][1] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+        if board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O":
+            print("Player 1 Wins!\n")
+            print("Congratulations!\n")
+            return True
+
+        for a in range(3):
+            for b in range(3):
+                if board[a][b] == "X" or board[a][b] == "O":
+                    count += 1
+                if count == 9:
+                    print("The game ends in a Tie\n")
+                    return True
+
+    while not end:
+        draw()
+        end = check_board()
+        if end == True:
+            break
+        print("Player 1 choose where to place a cross")
+        p1()
+        print()
+        draw()
+        end = check_board()
+        if end == True:
+            break
+        print("Player 2 choose where to place a nought")
+        p2()
+        print()
+
+    if input("Play again (y/n)\n") == "y":
+        print()
+        tic_tac_toe()
+
+tic_tac_toe()
