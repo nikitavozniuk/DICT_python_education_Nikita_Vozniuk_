8f2442f7198efa7204de90d01ee199aff93372cb
Nikita Vozniuk <n.i.voznyuk@student.khai.edu>
2021-12-04 17:54:05 +0200

    The 3-rd stage CreditCalculator

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,29 +1,74 @@
 import math
 
-choice_text = '''
-What do you want to calculate?
-type "m" – for number of monthly payments,
-type "p" – for the monthly payment:
+choice_text = '''What do you want to calculate?
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal:
 '''
 
 
-def CreditCalculator():
+def year_payment(li, p, loan):
+    i = (li * 0.01 / 12)
+    num = pow(1 + i, p)
+
+    return loan * ((i * num) / (num - 1))
+
+
+def loan_sum(li, ann, p):
+    i = (li * 0.01 / 12)
+    num = pow(1 + i, p)
+
+    return ann / ((i * num) / (num - 1))
+
+
+def payment_count(li, mp, loan):
+    i = (li * 0.01 / 12)
+    A = (mp / (mp - i * loan))
+
+    return math.ceil(math.log(A, i + 1))
+
+
+def n():
     loan = int(input('Enter the loan principal: '))
+    monthly_payment = int(input('Enter the monthly payment: '))
+    loan_interest = int(input('Enter the loan interest: '))
+
+    n = payment_count(loan_interest, monthly_payment, loan)
+
+    years = n / 12
+    month = (years - math.floor(years)) * 12
+    print('It will take {} years and {} months to repay this loan!'.format(math.floor(years), math.floor(month)))
+
+
+def a():
+    loan = int(input('Enter the loan principal: '))
+    periods = int(input('Enter the number of periods: '))
+    loan_interest = int(input('Enter the loan interest: '))
+
+    payment = math.ceil(year_payment(loan_interest, periods, loan))
+
+    print('Your monthly payment = {}!'.format(payment))
+
+
+def p():
+    annuity_payment = float(input('Enter the annuity payment: '))
+    periods = int(input('Enter the number of periods: '))
+    loan_interest = float(input('Enter the loan interest: '))
+
+    loan_principal = math.floor(loan_sum(loan_interest, annuity_payment, periods))
+
+    print('Your loan principal = {}!'.format(loan_principal))
+
+
+def CreditCalculator():
     choice = input(choice_text)
 
-    if choice == "m":
-        payment = int(input('Enter the monthly payment: '))
-        months = round(loan / payment)
-        print('It will take {} months to repay the loan'.format(months))
-    elif choice == "p":
-        month = int(input('Enter the number of months: '))
-        monthly_payment = math.ceil(loan / month)
-        last_payment = loan - (month - 1) * monthly_payment
-        if monthly_payment != last_payment:
-            print('Your monthly payment = {} and the last payment = {}'
-                  .format(monthly_payment, last_payment))
-        else:
-            print('Your monthly payment = {}'.format(monthly_payment))
+    if choice == 'n':
+        n()
+    elif choice == 'a':
+        a()
+    elif choice == 'p':
+        p()
 
 
 CreditCalculator()
