9142e046df62091f9a09936fa01f37d26ba4e3fe
Nikita <n.i.voznyuk@student.khai.edu>
2022-05-24 22:29:27 +0300

    The 2-nd stage arithmetic-test

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,12 +1,14 @@
-from math import radians
 import random
 import operator
 
-num1 = random.randint(2, 9)
-num2 = random.randint(2, 9)
+tries = 5
+count = 0
+correct = 0
 
 success = "Right!"
 fail = "Wrong!"
+type_fail = "Incorrect format."
+end_message = "Your mark is"
 
 ops_list = ["+", "-", "*"]
 
@@ -16,14 +18,25 @@ ops = {
     "*" : operator.mul,
 }
 
-rand_op = random.choice(ops_list)
+while tries > count:
+    num1 = random.randint(2, 9)
+    num2 = random.randint(2, 9)
+    rand_op = random.choice(ops_list)
+    correct_answer = ops[rand_op](num1, num2)
 
-correct_answer = ops[rand_op](num1, num2)
+    print(f"{num1} {rand_op} {num2}")
+    
+    try:
+        input_number = int(input())
 
-print(f"{num1}{rand_op}{num2}")
-input_number = int(input())
+        if (input_number == correct_answer):
+            print(success)
+            correct += 1
+            count += 1
+        else:   
+            print(fail)
+            count += 1
+    except ValueError:
+        print(type_fail)
 
-if (input_number == correct_answer):
-    print(success)
-else:
-    print(fail)
+print(f"{end_message} {correct}/{tries}")
