bbbe24b26e683451a0aaa2a5a595837ca8839e92
Nikita <n.i.voznyuk@student.khai.edu>
2022-05-25 21:14:54 +0300

    The 3-rd stage web_page_scraper

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -1,5 +1,5 @@
+import os
 import requests
-from bs4 import BeautifulSoup
 
 headers = {
     'Accept-Language': 'en-US,en;q=0.5',
@@ -13,13 +13,10 @@ headers = {
 class WebScrapper:
     def makeRequest(self, method, url, query_params=None, body=None, headers=None):
         try:
-            if "imdb.com/title" in url:
-                response = requests.request(method, url, params=query_params, json=body, headers=headers)
-            else:
-                print("Invalid quote resource!")
+            response = requests.request(method, url, params=query_params, json=body, headers=None)
+            if response.status_code == 400 or response.status_code == 404:
+                print(f"The URL returned {response.status_code}!")
                 raise SystemExit
-            if response.status_code == 400:
-                print("Invalid quote resource!")
 
             try:
                 if response.content:
@@ -30,42 +27,24 @@ class WebScrapper:
                 raise SystemExit
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
-
-    def parseImdbDescription(self, description):
-        appears = 0
-        i_slice = 0
-
-        for index, c in enumerate(description):
-            if c == ".":
-                appears += 1
-                if appears == 2:
-                    i_slice = index
-
-        return description[i_slice+2:]
-
-    def parseImdbTitle(self, title):
-        i_index = title.index('(')
-
-        return title[:i_index-1]
         
 
-
 def main():
-    web_scraper = WebScrapper()
+    webScrapper = WebScrapper()
     url = input("Input the URL: ")
-    req = web_scraper.makeRequest("GET", url, headers=headers)
-    soup = BeautifulSoup(req.content, 'html.parser')
+    page_content = webScrapper.makeRequest("GET", url)
 
-    info = dict()
-
-    title = soup.find('title')
-
-    description = soup.find('meta', { 'name': 'description' })
-
-    info["title"] = web_scraper.parseImdbTitle(title.text)
-    info["description"] = web_scraper.parseImdbDescription(description["content"])
-
-    print(info)
+    file = None
+    try:
+        file = open(f"{os.getcwd()}\web_page_scraper\source.html", 'wb')
+    except IOError:
+        print("Unable to create file on disk.")
+        file.close()
+        return
+    finally:
+        file.write(page_content.content)
+        print("Content saved.")
+        file.close()
     
 
 if __name__ == "__main__":
